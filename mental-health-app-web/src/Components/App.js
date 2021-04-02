576b3b630800ce02023c71fde2c63d923e9a9f37
Sriram2001 <sriramudupi19@gmail.com>
2021-04-03 01:15:28 +0530

    Deploy to github pages

--- mental-health-app-web/src/Components/App.js
+++ mental-health-app-web/src/Components/App.js
@@ -1,27 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NewPost from './Pages/NewPost';
-import Home from './Pages/Home';
-import SignIn from './Pages/SignIn';
-import SignUp from './Pages/SignUp';
-import AuthProvider from '../Context/AuthContext';
-import Nav from './Shared/Nav';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import NewPost from "./Pages/NewPost";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import AuthProvider from "../Context/AuthContext";
+import Nav from "./Shared/Nav";
 
 function App() {
-  return (
-    <div className="App">
-      <AuthProvider>
-        <BrowserRouter>
-        <Nav />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/sign_in" element={<SignIn />} />
-            <Route path="/sign_up" element={<SignUp />} />
-            <Route path="new/*" element={<NewPost />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-    </div>
-  );
+    return (
+        <div className="App">
+            <AuthProvider>
+                <HashRouter>
+                    <Nav />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/sign_in" element={<SignIn />} />
+                        <Route path="/sign_up" element={<SignUp />} />
+                        <Route path="new/*" element={<NewPost />} />
+                    </Routes>
+                </HashRouter>
+            </AuthProvider>
+        </div>
+    );
 }
 
 export default App;
